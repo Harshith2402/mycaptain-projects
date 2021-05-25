8455ca83f536e641756c46f8c15ae9c67dd01ce3
Harshith2402 <83497252+Harshith2402@users.noreply.github.com>
2021-05-25 18:30:23 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,5 +1,19 @@
-radius=r=1.1
+#python program to find area of a circle
+
+r=float(input('enter the radius of circle: ')) #getting input from the user
+
 import math
-math.pi
-A=(math.pi)*r**2
-print('Area of circle is: ', A)
+math.pi  #for getting the exact value of pi , I had imported math module
+
+a=(math.pi)*r**2 #assigning the formula
+
+print('Area of circle is: ', a)
+
+
+#python program to accept a filename and its extension
+
+file = input('enter your file name: ') #taking input from user 
+
+fn = f.split('.') #using split function to find the extension
+
+print('extension of file is : ' +fn[-1] , " 'python' ") #printing the extension
