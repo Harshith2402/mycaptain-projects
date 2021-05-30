c184fdd2367556b36af1c55007e0b8e3fc1c565c
Harshith2402 <83497252+Harshith2402@users.noreply.github.com>
2021-05-30 19:59:28 +0530

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -1,10 +1,15 @@
 #program to obtain positive numbers in a given range
 
-a=[1,2,3,-7,-7562,-4852,2584563,12365]
+print('\n enter your elements by giving space to each element')     #instructing user to give the input in correct format
 
-for i in range(0,len(a)) :
-    if a[i]>0 :
-       print(a[i])
-        
+elements = input('\n The elements required in your list:')          #taking input from the user to make a list
+
+a = (elements.split( ))                                             # using split function to develop required list
+
+print(a)                                                            
+
+for i in range(0 , len(a)) :                                        #using 'for loop' to encounter each element in the list
     
+    if float(a[i])>0 :                                              #converting each element from 'string' to 'float' for applying mathematical operations
+       print((a[i]))
 
